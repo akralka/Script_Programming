cff661d13a125532748e21b65a18c6e6d05dc221
akralka <akralka@student.agh.edu.pl>
2022-10-14 21:54:02 +0200

    nic

--- lab_1/tmp.py
+++ lab_1/tmp.py
@@ -1,30 +1,90 @@
-def avg(n):
-    avg = 0
-    for i in range(0,n):
-        avg += arr[i]
-    return avg
-def med(n):
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'quartiles' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+
+def quartiles(arr):
+    n = len(arr)
+    arr.sort()
+    tab = []
+    q = []
+    w = []
+    print(arr)
     if n % 2 == 0:
-        print((q[n // 2 - 1] + q[n //2]) / 2)
+        med2 = (arr[n//2 - 1] + arr[n//2]) / 2
+        for i in range(len(arr)):
+            if i < n//2:
+                q.append(arr[i])
+                if len(q) % 2 == 0:
+                    med1 = (q[len(q)//2 - 1] + q[len(q)//2]) / 2
+                else:
+                    med1 = q[len(q)//2]
+
+        for i in range(len(arr)):
+            if i > n//2 - 1:
+                w.append(arr[i])
+                print(w)
+                if len(w) % 2 == 0:
+                    med3 = (w[len(w)//2 - 1] + w[len(w)//2]) / 2
+                else:
+                    med3 = w[len(w)//2]
+        
+
     if n % 2 != 0:
-        print(q[n // 2])
-def mode(n):
-    a = 0
-    tmp = []
-    for i in range(0,n):
-        for j in range(0,n):
-            if i != j:
-                if arr[i] == arr[j]:
-                    a += 1
-                    tmp.append(arr[i])
-    print(tmp)
+        med2 = arr[n//2]
+        for i in range(len(arr)):
+            if i < n//2:
+                q.append(arr[i])
+                if len(q) % 2 == 0:
+                    med1 = (q[len(q)//2 - 1] + q[len(q)//2]) / 2
+                else:
+                    med1 = q[len(q)//2]
+
+        for i in range(len(arr)):
+            if i > n//2:
+                w.append(arr[i])
+                # print(w)
+                if len(w) % 2 == 0:
+                    med3 = (w[len(w)//2 - 1] + w[len(w)//2]) / 2
+                
+                else:
+                    med3 = w[len(w)//2]
+
+
+    tab.append(int(med1))
+    tab.append(int(med2))
+    tab.append(int(med3))
+
+
+    return tab
+
 
 
 if __name__ == '__main__':
-    n =int(input())
-    arr = list(map(int, input().rstrip().split())) 
-    q = sorted(set(arr))
-    print(float(avg(n)/10))
-    med(n)
-    mode(n)
     
+
+    n = int(input().strip())
+
+    data = list(map(int, input().rstrip().split()))
+
+    res = quartiles(data)
+
+    print(res)
+
+
+
+# 12
+# 4 17 7 14 18 12 3 16 10 4 4 12
+
+# 9
+# 3 7 8 5 12 14 21 13 18
