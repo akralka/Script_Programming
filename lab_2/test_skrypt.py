598eed33d762980939b8da9c92d879c8e0a82bdc
akralka <akralka@student.agh.edu.pl>
2022-10-15 19:38:02 +0200

    lab_2

--- lab_2/test_skrypt.py
+++ lab_2/test_skrypt.py
@@ -7,7 +7,7 @@ from fractions import Fraction
 
 class Test_TestSum(unittest.TestCase):
     def test_sum_integer_integer(self):
-        self.assertEqual(skrypt_2.digit('3ff4'), 'ff 34')   
+        self.assertEqual(skrypt_2.digit('3ff4'), 'ff   34')   
                                                 
 
 
