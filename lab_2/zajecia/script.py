496a7b4ab805c27a79700a86003d85f092be8409
akralka <akralka@student.agh.edu.pl>
2022-10-25 09:21:21 +0200

    logical_physical

--- lab_2/zajecia/script.py
+++ lab_2/zajecia/script.py
@@ -1,32 +1,52 @@
 import argparse, re
- 
-def open_file():
-    with open("file.py", "r") as file:
-        print(file.read()) 
-        # for el in file.readlines():
-        #     print(el, end="")
-        #     file.close()
 
+def open_file(file):
+    with open(file, "r") as f:
+        for i in f.readlines():
+            print(i, end="")
 
+def conversion(file, char):
 
+    new_file =  open('file_tmp.txt',"w")
 
+    string = ""
+
+    if char in "+*?.[]()\{\}$^\\|":
+        tmp = f"\\{char}$"
+    else:
+        tmp = f"{char}$"
+
+    file = open(file, 'r').read()
+    lines = file.split('\n')
+    for line in lines:
+        if re.search(tmp, line):
+            string += line[:-1]     
+            new_file.write(string)      
+        else:
+            string +=line 
+            new_file.write(string+'\n')
+        string = ""
+
+    new_file.close()
+    return open_file("file_tmp.txt")
 
 if __name__ == "__main__":
 
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", help=r"a continuation character for input files (default: '\')", required=False)
-    parser.add_argument("-f", "--file", help="Give input!", required=True)
+    parser.add_argument("-c", help=r"a continuation character for input files (default: '\')", required=False, default="\\")
+    parser.add_argument("-f", "--file", help="Give input!", required=True, nargs="+")
 
+    
     try:
         args = parser.parse_args()
         
     except:
         print("Specify file names")      
+        exit()
+
+    for file in args.file:
+        conversion(file, args.c[0])
 
 
-        # open_file()
-
-
-
+# python .\script.py -f file.py    
 
