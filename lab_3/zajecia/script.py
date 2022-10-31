50027454a43e681c14e1b39a334c34211ad53fe0
akralka <akralka@student.agh.edu.pl>
2022-10-31 14:05:16 +0100

    skrypt_3

--- lab_3/zajecia/script.py
+++ lab_3/zajecia/script.py
@@ -1,10 +1,21 @@
+import argparse
 
 class Library:
     def __init__(self):
-        self.books= {"brysiu": 5, "Random kic": 7, "still_stresujacy_marcel:(((": 0.75}
+        self.books= {"brysiu": 5, "Random kic": 7, "bardzo_pomocny_marcel:(((": 0.75}
         self.transactions = {} 
 
-    def __str__(self):
+    # def __str__(self):
+    #     return self.transactions
+
+    # def parseFileLine():
+    #     file = open("book.txt", 'r').read()
+    #     lines = file.split('\n')
+    #     for line in lines:
+    #         L.books = {line[0]: line[1]} 
+    #     return L.books
+
+    def parseInputLine():
         pass
     
     def borrow(self, book, quantity, name):
@@ -45,7 +56,7 @@ class Library:
         if book not in self.books.keys():
             return "Wrong book"
 
-        if isinstance(quantity, int):
+        if isinstance(quantity, str):
             if not quantity.isdigit():
                 return 'Wrong quantity'
 
@@ -59,18 +70,56 @@ class Library:
         if quantity <= 0:
             return "Quantity equal or less than 0"
 
+        
+
+        if name not in self.transactions.keys():
+            return "You didn't borrow anything"     
+        else:
+            if book not in self.transactions[name]:
+                return "You didn't borrow that book"
+            else:
+                if self.transactions[name][book] < quantity:
+                    return "You borrow less copies of that book"
+
         self.books[book] += quantity  
         
+        if name in self.transactions.keys():
+            if book in self.transactions[name]:
+                self.transactions[name][book] -= quantity
+            else:
+                self.transactions[name].update({book: quantity})
+        else:
+            self.transactions.update({name: {book: quantity}})
+
+
+if __name__ == '__main__':
+    
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("-f", "--file", help="Give input!", required=True, nargs="+")
+
+    # try:
+    #     args = parser.parse_args()
+        
+    # except:
+    #     print("Specify file names")      
+    #     exit()
+
+    # for file in args.file:
+    #     conversion(file, args.c[0])
     
 
+    L = Library()
 
+    while True:
+        try:
+            operation = input().split()
+            # transaction[0] to ma byc nazwa pliku
+            if operation[0] == 'borrow':
+                L.borrow(operation[1], operation[2], operation[3])
 
+            if operation[0] == 'book_return':
+                L.book_return(operation[1], operation[2], operation[3])
 
-# if __name__ == '__main__':
-#     while True:
-#         pass
+        except(EOFError):
+            pass
 
-
-
-
-# s ł o d z i a k :((
--- lab_3/zajecia/test_script.py
+++ lab_3/zajecia/test_script.py
@@ -22,22 +22,23 @@ class Test_TestBorrow(unittest.TestCase):
         self.assertEqual(script.Library().borrow('brysiu', -3, 'Nowak'), 'Quantity equal or less than 0')
 
 
-# class Test_TestRefund(unittest.TestCase):
+class Test_TestReturn(unittest.TestCase):
 
-#     def test_refund_wrong_item(self):
-#         self.assertEqual(script.refund('Brysiu', 12, 'Nowak'), 'Nieprawidłowy produkt\n')
+    def test_book_return_wrong_item(self):
+        self.assertEqual(script.Library().book_return('cos', 1, 'Nowak'), 'Wrong book')
 
-#     def test_refund_wrong_name(self):
-#         self.assertEqual(script.refund('T-shirt', 12, 'Nowak((1'), 'Wrong name')
+    def test_book_return_wrong_name(self):
+        self.assertEqual(script.Library().book_return('brysiu', 1, 'Nowak12['), 'Wrong name')
 
-#     def test_refundlack_of__name(self):
-#         self.assertEqual(script.refund('T-shirt', 1, ''), 'No name')
+    def test_book_returnlack_of__name(self):
+        self.assertEqual(script.Library().book_return('brysiu', 1, ''), 'No name')
 
-#     def test_refund_wrong_quantity(self):
-#         self.assertEqual(script.refund('T-shirt', '[aa]', 'Nowak'), 'Wrong quantity')
+    def test_book_return_wrong_quantity(self):
+        self.assertEqual(script.Library().book_return('brysiu', '[aa]', 'Nowak'), 'Wrong quantity')
 
-#     def test_refund_quantity_less_than_0(self):
-#         self.assertEqual(script.refund('T-shirt', -3, 'Nowak'), 'Quantity equal or less than 0')
+    def test_book_return_quantity_less_than_0(self):
+        self.assertEqual(script.Library().book_return('brysiu', -3, 'Nowak'), 'Quantity equal or less than 0')
+
 
 if __name__ == '__main__':
     unittest.main()
