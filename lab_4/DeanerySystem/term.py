44506843c3ab2a9c2cc42f17ef2bef9c692441c3
akralka <akralka@student.agh.edu.pl>
2022-11-07 15:19:46 +0100

    lab4_1

--- lab_4/DeanerySystem/term.py
+++ lab_4/DeanerySystem/term.py
@@ -1,18 +1,17 @@
 from day import Day
 
-
 class Term():
-    def __init__(self, day, hour, minute, duration):
-      self.hour = hour
-      self.minute = minute
-      self.duration = duration
-      self.__day = day
+    def __init__(self, day, hour, minute, duration = None):
+        self.hour = hour
+        self.minute = minute
+        if duration is not None:
+            self.duration = duration
+        else:
+            self.duration = 90
+        self.__day = day
 
     def __str__(self):
-        if self.duration == 0:
-            return f"{Term.method(self._Term__day.value)} {self.hour}:{self.minute} [90]"
-        else:
-            return f"{Term.method(self._Term__day.value)} {self.hour}:{self.minute} [{self.duration}]"
+        return f"{Term.method(self._Term__day.value)} {self.hour}:{self.minute} [{self.duration}]"
 
     def earlierThan(self, termin):
         if self.__day.value < termin.__day.value:
@@ -42,17 +41,6 @@ class Term():
             return True
         return False
 
-    # def tmp(self,termin):
-    #     if (Term(self.__day, self.hour, self.minute, self.duration).laterThan(Term(termin.__day, termin.hour, termin.minute, self.duration))):
-    #         return False
-    #     if (Term(self.__day, self.hour, self.minute, self.duration).laterThan(Term(termin.__day, termin.hour, termin.minute, self.duration))):
-    #         if self.duration != termin.duration:
-    #             return True
-    #         else:
-    #             return False
-    #     else:
-    #         return True
-
     @staticmethod
     def method(day):
         if day == 1:
@@ -69,23 +57,55 @@ class Term():
             return "SOBOTA"
         elif day == 7:
             return "NIEDZIELA"
+        
+     #  >
+    def __gt__(self, termin):
+        return self.laterThan(termin)
 
+     #  < 
+    def __lt__(self, termin):
+        return self.earlierThan(termin)
+
+    #  <= 
+    def __le__(self, termin):
+        if self.equals(termin):
+            return True
+        else:
+            return self.earlierThan(termin)
+
+    # >=
+    def __ge__(self, termin):
+        if self.equals(termin):
+            return True
+        else:
+            return self.laterThan(termin)
+    
+      # == 
+    def __eq__(self, termin):
+        if self.duration == termin.duration:
+            return self.equals(termin)
+        else:
+            return False
+
+    def __sub__(self, termin):
+        time_2day = ((termin.__day.value)-1) *24 *60 + termin.hour * 60 + termin.minute
+        time_1day = ((self.__day.value)-1) *24 *60 + self.hour * 60 + self.minute
+        return f"{Term.method(termin.__day.value)} {termin.hour}:{termin.minute} [{time_1day - time_2day + self.duration}]"
     
 
 if __name__ =='__main__':
 
-    term1 = Term(Day.MON, 8, 30, 0)
+    term1 = Term(Day.MON, 8, 30)
     term2 = Term(Day.TUE, 9, 45, 30)
     term3 = Term(Day.TUE, 9, 45, 90)
     print(term1)
     print(term2)
-    print(term3)
-    # print(term1.earlierThan(term2)) 
-    # print(term1.laterThan(term2))
-    # print(term1.equals(term2))
-    # print(Term.tmp(term1, term2))
-    # print(Term.tmp(term2, term1))
-    # print(Term.tmp(term2, term3))
-
-    
-    
+    print(term3)     
+    print("term1 < term2:", term1 < term2)   # Ma się wypisać True
+    print("term1 <= term2:", term1 <= term2) # Ma się wypisać True
+    print("term1 > term2:", term1 > term2)   # Ma się wypisać False
+    print("term1 >= term2:", term1 >= term2) # Ma się wypisać False
+    print("term2 == term2:", term2 == term2) # Ma się wypisać True
+    print("term2 == term3:", term2 == term3) # Ma się wypisać False
+    term4 = term3 - term1                    
+    print(term4)                                      
---
+++ lab_4/DeanerySystem/tmp.py
@@ -0,0 +1,11 @@
+
+
+    # overload + operator
+    def __add__(self, point_ov):
+        return Point(self.__xCoord + point_ov.__xCoord, self.__yCoord + point_ov.__yCoord)
+
+    # overload - operator
+    
+    
+   
+  
