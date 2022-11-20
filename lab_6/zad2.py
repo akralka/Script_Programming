7c8398517b70e9a5ec1fbe6acb402fd0fe2198f3
akralka <akralka@student.agh.edu.pl>
2022-11-20 14:30:08 +0100

    modified

--- lab_6/zad2.py
+++ lab_6/zad2.py
@@ -1,6 +1,6 @@
 # python3 -c 'from collections import Counter;import sys,re;res = dict(Counter((map(lambda i:len(i), (re.findall(f"\\w+", sys.stdin.read()))))));print(res)'
 
-
+# dla czytelnosci
 from collections import Counter
 import sys
 import re
--- lab_6/zad3.py
+++ lab_6/zad3.py
@@ -1,5 +1,12 @@
-
 python3 -c "import itertools, sys; array = list(itertools.chain(iter(open(sys.argv[1])), iter(open(sys.argv[2]))));tab = [int(num) for line in array for num in line.rstrip().split()];print (len(list(itertools.filterfalse(lambda x : x % 2 != 0, tab ))))" "plik1.txt" "plik2.txt"
+
+
+
+import itertools, sys
+
+array = list(itertools.chain(iter(open(sys.argv[1])), iter(open(sys.argv[2]))))
+tab = [int(num) for line in array for num in line.rstrip().split()]
+print (len(list(itertools.filterfalse(lambda x : x % 2 != 0, tab ))))
 
 
 
@@ -17,7 +24,6 @@ python3 -c "import itertools, sys; array = list(itertools.chain(iter(open(sys.ar
 # print(len(tab))
 
 #  python3 zad3.py "plik1.txt" "plik2.txt"
-
 
 
 
