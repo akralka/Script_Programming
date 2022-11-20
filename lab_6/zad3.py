407a42e05d06f74ba7a229752c395aedc62dba6e
akralka <akralka@student.agh.edu.pl>
2022-11-20 14:22:47 +0100

    zad3.py

--- lab_6/zad3.py
+++ lab_6/zad3.py
@@ -1,40 +1,27 @@
-# python3 -c "from functools import reduce;import itertools,re,sys;res = reduce(lambda val, x: val+((x-1) % 2), iter(map(int, re.findall('\\d+', reduce(lambda val,e: val+e, itertools.chain(iter(open(sys.argv[1])), iter(open(sys.argv[2]))))))),0);print(res)", "plik1.txt" "plik2.txt"
+
+python3 -c "import itertools, sys; array = list(itertools.chain(iter(open(sys.argv[1])), iter(open(sys.argv[2]))));tab = [int(num) for line in array for num in line.rstrip().split()];print (len(list(itertools.filterfalse(lambda x : x % 2 != 0, tab ))))" "plik1.txt" "plik2.txt"
 
 
 
-# from functools import reduce;import itertools,re,sys;
-# res = reduce(lambda val, x: val + ((x-1) % 2), iter(map(int, re.findall('\\d+', 
-# reduce(lambda val,a: a+val, itertools.chain(iter(open(sys.argv[1])), iter(open(sys.argv[2]))))))),0);print(res)
+
+# import sys, functools
+# files = sys.argv[1:]
+# tab = [int(num) for file in files for line in open(file, 'r').readlines() for num in line.rstrip().split()]
+# for file in files:
+#     with open(file, 'r') as File:
+#         for line in File:
+#             line = line.rstrip().split()
+#             for i in line:
+#                 if int(i) % 2 == 0:
+#                     tab.append(i)
+# print(len(tab))
+
+#  python3 zad3.py "plik1.txt" "plik2.txt"
 
 
 
-# python3 -c "from functools import reduce;import itertools,re,sys;res = reduce(lambda val, x: val+((x-1) % 2), 
-# iter(map(int, re.findall('\\d+', reduce(lambda val,e: val+e, itertools.chain(iter(open(sys.argv[1])), iter(open(sys.argv[2]))))))),0);
-# print(list(itertools.chain(iter(open(sys.argv[1])), iter(open(sys.argv[2])))))", "plik1.txt" "plik2.txt"
 
-
-# num = [1, 3, 5, 6, 2]
-# from functools import reduce;import itertools,re,sys;res = reduce(lambda val, x: x+val, num); print(res)
+# reduce()--------------------------------------------------------------
 # import functools
 # lis = [1, 3, 5, 6, 2]
 # print(functools.reduce(lambda a, b: a+b, lis)) >> sume z lis
-
-import sys, functools
-nums = sys.argv[1:]
-tab = []
-a=0
-for i in nums:
-    file = i
-    with open(file, 'r') as File:
-        for line in File:
-            line = line.rstrip().split()
-            for i in line:
-                i = int(i)
-                if i % 2 == 0:
-                    tab.append(i)
-res = functools.reduce(lambda x, val:val%2 ==0, tab)
-print(res)
-#         a+=len(tab)
-#         tab = []
-# print(a)
-#  python3 zad3.py "plik1.txt" "plik2.txt"
