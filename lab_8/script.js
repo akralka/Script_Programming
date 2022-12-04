125e6810ffded938b75edb727a3732673ddc8469
akralka <akralka@student.agh.edu.pl>
2022-12-04 19:01:16 +0100

    script.js

--- lab_8/script.js
+++ lab_8/script.js
@@ -27,15 +27,14 @@ function suma(napis){
     if(typeof(suma.suma) == 'undefined'){
         suma.suma = 0
     }
-    var num = ""
-    for (const char of napis){
-        if(isNaN(Number(char))){
-            break;
+    var res = ""
+    for (let string of napis){
+        if(!isNaN(Number(string))){
+            res += string
         }else{
-            num += char
-        }
-    }
-    suma.suma += Number(num)
+            break;
+        }}
+    suma.suma += Number(res)
     return suma.suma
 }
 
@@ -45,36 +44,37 @@ describe('All possible combinations of string', function() {
     it('Numbers', function() {
         expect(cyfry("2022")).to.equal(6);
         expect(litery("2022")).to.equal(0);
-        // expect(suma("2022")).to.equal(0);
+        expect(suma("2022")).to.equal(2022);
     });
     
     it('Letters', function() {
         expect(cyfry("str")).to.equal(0);
         expect(litery("str")).to.equal(3);
-        // expect(suma("str")).to.equal(0);
+        expect(suma("str")).to.equal(2022);
     });
 
     it('Letters and numbers', function() {
         expect(cyfry("str2022")).to.equal(6);
         expect(litery("str2022")).to.equal(3);
-        // expect(suma("str2022")).to.equal(0);
+        expect(suma("str2022")).to.equal(2022);
     });
 
     it('Numbers and letters', function() {
         expect(cyfry("2022str")).to.equal(6);
         expect(litery("2022str")).to.equal(3);
-        // expect(suma("2022str")).to.equal(0);
+        expect(suma("2022str")).to.equal(4044);
     });
 
     it('No letters and no numbers', function() {
         expect(cyfry("")).to.equal(0);
         expect(litery("")).to.equal(0);
-        // expect(suma("")).to.equal(0);
+        expect(suma("")).to.equal(4044);
     }); 
 });
 
 var value = window.prompt()
 while(value != null){
+    // console.log(suma(value));
     document.write("W stringu  ", value, "  suma cyfr to:  ", cyfry(value),", liczba liter to:  ",litery(value))
     document.write("<br>");
     value = window.prompt()
