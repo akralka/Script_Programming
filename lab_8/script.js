563dacebef798eb8b71be94a1c65948eb43abeb9
akralka <akralka@student.agh.edu.pl>
2022-12-04 15:38:07 +0100

    modified lab 8

--- lab_8/script.js
+++ lab_8/script.js
@@ -22,31 +22,60 @@ function cyfry(napis){
     return res
 }
 
+
+function suma(napis){
+    if(typeof(suma.suma) == 'undefined'){
+        suma.suma = 0
+    }
+    var num = ""
+    for (const char of napis){
+        if(isNaN(Number(char))){
+            break;
+        }else{
+            num += char
+        }
+    }
+    suma.suma += Number(num)
+    return suma.suma
+}
+
 var expect = chai.expect;
 
 describe('All possible combinations of string', function() {
     it('Numbers', function() {
         expect(cyfry("2022")).to.equal(6);
         expect(litery("2022")).to.equal(0);
-
+        // expect(suma("2022")).to.equal(0);
     });
     
     it('Letters', function() {
         expect(cyfry("str")).to.equal(0);
         expect(litery("str")).to.equal(3);
-
+        // expect(suma("str")).to.equal(0);
     });
 
     it('Letters and numbers', function() {
         expect(cyfry("str2022")).to.equal(6);
         expect(litery("str2022")).to.equal(3);
+        // expect(suma("str2022")).to.equal(0);
+    });
 
+    it('Numbers and letters', function() {
+        expect(cyfry("2022str")).to.equal(6);
+        expect(litery("2022str")).to.equal(3);
+        // expect(suma("2022str")).to.equal(0);
     });
 
     it('No letters and no numbers', function() {
         expect(cyfry("")).to.equal(0);
         expect(litery("")).to.equal(0);
-
+        // expect(suma("")).to.equal(0);
     }); 
 });
 
+var value = window.prompt()
+while(value != null){
+    document.write("W stringu  ", value, "  suma cyfr to:  ", cyfry(value),", liczba liter to:  ",litery(value))
+    document.write("<br>");
+    value = window.prompt()
+}
